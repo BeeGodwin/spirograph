2d1f10df76d7da02b8d06b91f8bc1781baa26ac8
JaminaBGodwin <bgodwi01@dcs.bbk.ac.uk>
2018-01-01 02:00:00 -0500

    basic spirograph works- we have math!

--- play_spiro.py
+++ play_spiro.py
@@ -1,21 +1,33 @@
 import pygame
 import sys
 from pygame.locals import *
-import random
+from spirograph import Circle
 
 
 def main():
     pygame.init()
 
     clock = pygame.time.Clock()
-    fps = 60
+    fps = 180
 
     res = wid, hi = 1440, 870
     d_surf = pygame.display.set_mode(res)
 
+    outer_c = Circle(300, is_active=True, point=100)
+    outer_c.set_position(wid // 2, hi // 2)
+
+    inner_c = Circle(36, is_active=True, point=85)
+    inner_c.set_position(outer_c.x, outer_c.y - outer_c.radius + inner_c.radius)
+
+    outer_c.add_child(inner_c)
+
     while True:
 
-        # d_surf.fill((0, 0, 0))
+        # p_x, p_y = outer_c.get_point_position()
+        # d_surf.set_at((int(p_x), int(p_y)), (255, 255, 255))
+        outer_c.inc_angle(1)
+        p1_x, p1_y = inner_c.get_point_position()
+        d_surf.set_at((int(p1_x), int(p1_y)), (255, 255, 255))
 
         for e in pygame.event.get():
             if e.type == QUIT or e.type == KEYDOWN and e.key == 27:
--- spirograph.py
+++ spirograph.py
@@ -1,4 +1,4 @@
-from math import sin, cos, degrees, radians
+from math import sin, cos, degrees, radians, pi
 
 
 class Circle:
@@ -10,9 +10,23 @@ class Circle:
         self.point = point  # expressed as a percentage of self.radius
         self.x = 0
         self.y = 0
+        self.children = []
 
     def inc_angle(self, deg):
         self.angle += deg
+        for child in self.children:
+            child.inc_angle(-(self.radius / child.radius) * deg)
+            self.set_child_position()
+
+    def set_child_position(self):
+        norm_x, norm_y = normalised_vector(self.angle)
+        for child in self.children:
+            radius = self.radius - child.radius
+            ch_x = self.x + norm_x * radius
+            ch_y = self.y + norm_y * radius
+            child.set_position(ch_x, ch_y)
+            # set the child's position, using self.angle, self.radius, and self.x , y.
+
 
     def set_position(self, x, y):
         self.x = x
@@ -27,10 +41,13 @@ class Circle:
         norm_y *= point_rad
         return norm_x + self.x, norm_y + self.y
 
+    def add_child(self, child):
+        self.children.append(child)
+
 
 def normalised_vector(deg):
-    """given an angle in degrees, where 0 is down, return a
+    """given an angle in degrees, where 0 is up, return a
     normalised vector as the 2-tuple (x,y)."""
     vec_x = sin(radians(deg))
-    vec_y = cos(radians(deg))
+    vec_y = -cos(radians(deg))
     return round(vec_x, 3), round(vec_y, 3)
